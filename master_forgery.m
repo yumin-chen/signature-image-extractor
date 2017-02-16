54d884bb84c442f08f9c7ea3ff03c62f5a7eaf94
CharmySoft <charmysoft@gmail.com>
2017-02-16 17:10:08 +0000

    Cropping

--- master_forgery.m
+++ master_forgery.m
@@ -21,41 +21,55 @@ close all;  % Close all sub-windows
 % Read image
 image = im2double(imread('Boss.bmp'));
 gray = rgb2gray(image); 
+[height, width, depth] = size(image);
 
-% Strech contrast
+
+% ------------------------
+% Stretch contrast
+% ------------------------
 gMin = min(gray(:));
 gMax = max(gray(:));
 gStd = std(gray(:));
+% Apply statistics three-sigma rule to bound to 99.73% of data
 gLowerBound = max(gMin, (gMax + gMin) / 2 - gStd * 3);
 gUpperbound = min(gMax, (gMax + gMin) / 2 + gStd * 3);
-contrastStretched = (gray - gLowerBound) / (gUpperbound - gLowerBound);
+contrastStretched = (image - gLowerBound) / (gUpperbound - gLowerBound);
 
-threshold = mean(contrastStretched(:));
-blackWhite = double(contrastStretched > threshold);
-seg = gray .* blackWhite;
-colored = image;
 
-[height, width, depth] = size(image);
+% ------------------------
+% Adaptive thresholding
+% ------------------------
+adaptiveThresholding = image;
 half = 3;
+% Loop through the image to apply threshold to each pixel
 for x = half + 1:width - half
     for y = half + 1: height - half
-        area = contrastStretched(y-half:y+half, x-half:x+half);
+        area = contrastStretched(y-half:y+half, x-half:x+half, :);
         threshold = mean(area(:)) - 0.05;
-        seg(y, x) = double(contrastStretched(y, x) > threshold);
-        if contrastStretched(y, x) > threshold
-            colored(y, x, :) = 1;
+        if mean(contrastStretched(y, x, :)) > threshold
+            % Wipe out the pixel if larger than threshold
+            adaptiveThresholding(y, x, :) = 1;
         end            
     end
 end
+% Show adaptive thresholding reconstructed image
+figure, imshow(adaptiveThresholding), title('Adaptive Thresholding');
 
 
-figure;
-imshow(contrastStretched), title('Contrast Stretched');
-%subplot(1,3,2), imshow(blackWhite), title('Global Thresholding');
-figure;
-imshow(seg), title('Adaptive Thresholding');
-figure;
-imshow(colored), title('Colored');
-
+% ------------------------
+% Cropping
+% ------------------------
+% Convert to gray and reverse to a black and white binary image
+binaryImage = rgb2gray(adaptiveThresholding(half + 1: height - half, ...
+    half + 1:width - half, :)) ~= 1;
+% Find the non-zero areas
+[rows, columns] = find(binaryImage);
+top = min(rows);
+bottom = max(rows);
+left = min(columns);
+right = max(columns);
+croppedImage = adaptiveThresholding(top:bottom, left:right, :);
+% Show cropped image
+figure, imshow(croppedImage), title('Cropped image');
 
 
